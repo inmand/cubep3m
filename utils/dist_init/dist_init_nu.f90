cfc1a96d0c5af396d978b4015be3c6930821b2e1
inmand <inmand@cita.utoronto.ca>
2014-01-13 19:15:15 -0500

    fix bug

--- utils/dist_init/dist_init_nu.f90
+++ utils/dist_init/dist_init_nu.f90
@@ -1034,7 +1034,9 @@ contains
     rank_s=adjustl(rank_s)
     if (nu_flag .AND. nu_dist) then
         fn=scratch_path//'xv'//rank_s(1:len_trim(rank_s))//'_nu.ic'
-    else 
+    else if (nu_flag)
+        fn=scratch_path//'xv'//rank_s(1:len_trim(rank_s))//'_nu.ic'
+    else
         fn=scratch_path//'xv'//rank_s(1:len_trim(rank_s))//'.ic'
     endif
     open(11,file=fn,form=IOform,iostat=ioerr)
